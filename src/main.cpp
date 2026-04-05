1b78ddbb3bc8162b0374cb487c968b53ab06b2c3
Seth McNeill <mcneills@morningside.edu>
2026-04-05 09:33:51 -0500

    Spinlocks working with Serial demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,34 @@
 #include <Arduino.h>
 
-auto_init_mutex(my_mutex);  // Create a mutex lock
+spin_lock_t *lock;  // store lock here
 
 void setup() {
   Serial.begin(4800);
+  // Claim a spinlock (0-15)
+  uint32_t lock_num = spin_lock_claim_unused(true);
+  lock = spin_lock_instance(lock_num);
+
   delay(3000);
   Serial.println("Core 1 starting");
+  Serial.println("Using spinlock #" + String(lock_num));
+  delay(5000);
 }
 
 void loop() {
-  mutex_enter_blocking(&my_mutex);  // check out mutex
+  spin_lock_unsafe_blocking(lock);  // does not prevent deadlocks (lock already checked out by this core)
   // Serial.println("1");
-  Serial.println("1111111111111111111111111111111");
-  mutex_exit(&my_mutex);  // return mutex
+  Serial.println("1111111111111111111111111111111");  // starts with more of these since loop starts before loop1 due to delays
+  spin_unlock_unsafe(lock);
 }
 
 
 void setup1() {
-  delay(4000);
+  delay(10000);
   Serial.println("Core 2 starting");
 }
 
 void loop1() {
-  mutex_enter_blocking(&my_mutex);  // check out mutex
+  spin_lock_unsafe_blocking(lock);  // does not prevent deadlocks (lock already checked out by this core)
   Serial.println("-------------------------------");
-  mutex_exit(&my_mutex); // return mutex
+  spin_unlock_unsafe(lock);  // release lock
 }
